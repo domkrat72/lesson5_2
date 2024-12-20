42d8f86e0631a70ce2b9fbb254f67a9b99bbb4e9
domkrat72 <snovikov72@gmail.com>
2024-12-20 14:34:00 +0300

    Add files via upload

---
+++ lesson5_2.py
@@ -0,0 +1,30 @@
+class House:
+    pass
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+print(len(h1))
+print(len(h2))
